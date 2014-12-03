b2909f045c05be4bb1ed77b34415098de11018b9
Dllieu <dllieu@hotmail.com>
2014-12-03 20:15:19 +0800

    todo / FUNCTOR TEst suite

---
+++ source/tests/testsuite/FunctorTestSuite.cpp
@@ -0,0 +1,5 @@
+//--------------------------------------------------------------------------------
+// (C) Copyright 2014-2015 Stephane Molina, All rights reserved.
+// See https://github.com/Dllieu for updates, documentation, and revision history.
+//--------------------------------------------------------------------------------
+#include <boost/test/unit_test.hpp>
