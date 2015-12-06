95d56aa46fe9357902ee2e6ff9683bd452e19b3b
Dllieu <molina.stephan@gmail.com>
2015-12-06 16:01:36 +0800

    StreamSiTest

--- source/testsuite/CoroutineTestSuite.cpp
+++ source/testsuite/CoroutineTestSuite.cpp
@@ -12,12 +12,9 @@ BOOST_AUTO_TEST_SUITE( CoroutineTestSuite )
 namespace
 {
     // If a coroutine has a declared return type that contains a placeholder type, then the return type of the coroutine is deduced as follows :
-    //  - If a yield - keyword statement and an await - expression are present, then the return type is std::experimental::async_stream<T>
-    //    where T is deduced from the yield - keyword statements as if a yield - keyword statement were a return statement in a function with declared type auto without a trailing - return-type.
-    //  - Otherwise, if an await - expression is present in a function, then the return type is std::experimental::task<T> where type T is deduced from coroutine,
-    //    return-keyword statements as if a coroutine - return-keyword statement were a return statement in a function with declared type auto without a trailing - return-type.
-    //  - Otherwise, if a yield - keyword statement is present in a function, then the return type is std::experimental::generator<T>, where T is deduced from the yield - keyword statements,
-    //    as if a yield - keyword statement were a return statement in a function with declared type auto without a trailing - return-type.
+    //  - If a yield statement and either an await expression or an await-for statement are present, then the return type is std::experimental::async_stream<T >, where T is deduced from the yield statements.
+    //  - Otherwise, if an await expression or an await-for statement are present in a function, then the return type is std::experimental::task<T> where type T is deduced from return statements
+    //  - Otherwise, if a yield statement is present in a function, then the return type is std::experimental::generator<T>
     /*std::experimental::generator<int>*/auto   generate_index( size_t to )
     {
         auto i = 0;
@@ -42,7 +39,6 @@ namespace
 BOOST_AUTO_TEST_CASE( GeneratorTest )
 {
     std::array< int, 7 > fibo_result{ 0, 1, 1, 2, 3, 5, 8 };
-
     auto i = 0;
 
     // std::experimental::generator<int>::iterator::operator* will call promise()._CurrentValue
--- source/testsuite/SIMDTestSuite.cpp
+++ source/testsuite/SIMDTestSuite.cpp
@@ -3,14 +3,61 @@
 // See https://github.com/Dllieu for updates, documentation, and revision history.
 //--------------------------------------------------------------------------------
 #include <boost/test/unit_test.hpp>
+#include <array>
+#include <cstdlib>
 
-BOOST_AUTO_TEST_SUITE( SIMDTestSuite )
+#include "tools/Timer.h"
 
 // Intrinsics resemble assembly language except that they leave the actual register allocation, instruction scheduling, and addressing modes to the compiler.
 // Except for explicit unaligned load and store, compiler assume that packed memory operands of instrinsics are properly aligned
-BOOST_AUTO_TEST_CASE( asdasd )
+BOOST_AUTO_TEST_SUITE( SIMDTestSuite )
+
+namespace
 {
-    // TODO
+    template < size_t SIZE >
+    void    copy_with_offset( std::array< int, SIZE >& to, const std::array< int, SIZE >& from, int offset )
+    {
+        static_assert( SIZE % 16 == 0, "size not multiple of 16" );
+
+        // loop is memory bound if array not in the cache because it does nothing else that is time-consuming
+        std::transform( from.begin(), from.end(), to.begin(), [ &offset ]( auto n ){ return n + offset; } );
+    }
+
+    template < size_t SIZE >
+    void    copy_with_offset_with_simd( std::array< int, SIZE >& to, const std::array< int, SIZE >& from, int offset )
+    {
+        static_assert( SIZE % 16 == 0, "size not multiple of 16" );
+
+        auto to4 = reinterpret_cast< __m128i* >( to.data() );
+        auto from4 = reinterpret_cast< const __m128i* >( from.data() );
+        // Sets the 4 signed 32-bit integer values in reverse order
+        auto offset4 = _mm_setr_epi32( offset, offset, offset, offset );
+
+        // still memory bounded, additional improvement that could be done:
+        //   - more work in the loop using the time being wasted for memory
+        //   - reducing the amount of memory used which also reduced the number of cache misses
+        //   - making sure the memory was in the cache (by using a strip-mining technique with another function that accessed the same memory)
+        for ( int i = 0; i < SIZE / 4; ++i )
+            // void _mm_stream_si128(__m128i *p, __m128i a) : Stores the data in a to the address p without polluting the caches.
+            //                                                If the cache line containing address p is already in the cache, the cache will be updated. Address p must be 16 byte aligned.
+            _mm_stream_si128( to4 + i, _mm_add_epi32( from4[i], offset4 ) /* Adds the 4 signed or unsigned 32-bit integers in from4[i] to the 4 signed or unsigned 32-bit integers in offset4 */);
+    }
+}
+
+BOOST_AUTO_TEST_CASE( StreamSiTest )
+{
+    constexpr size_t size = 16 * 100;
+    constexpr int offset = 54;
+    std::array< int, size > from;
+    std::generate( from.begin(), from.end(), std::rand /*C rand*/ );
+
+    std::array< int, size > to;
+    std::array< int, size > toSimd;
+    auto elapsedNaive = tools::Timer::elapsed( [ &to, &from, &offset ] { copy_with_offset( to, from, offset ); } );
+    auto elapsedSimd = tools::Timer::elapsed( [ &toSimd, &from, &offset ] { copy_with_offset_with_simd( toSimd, from, offset ); } );
+
+    BOOST_CHECK( to == toSimd );
+    BOOST_CHECK( elapsedNaive >= elapsedSimd );
 }
 
 BOOST_AUTO_TEST_SUITE_END() // SIMDTestSuite
